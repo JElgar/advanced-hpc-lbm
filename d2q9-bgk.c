7d8ed41c081ea69741fb7ef07ab9bf9f3a4dea25
jelgar <jamesnelgar@gmail.com>
2021-02-22 21:01:25 +0000

    Added omp parallel for

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -248,6 +248,7 @@ float propagate_rebound_and_collisions(const t_param params, t_speed* restrict c
 
   /* loop over _all_ cells */
   #pragma omp simd
+  #pragma omp parallel for
   for (int jj = 0; jj < params.ny; jj++)
   {
     for (int ii = 0; ii < params.nx; ii++)
