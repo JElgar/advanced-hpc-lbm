269a163cf33769d8fa24a25c94a522fb5b8a4462
jelgar <jamesnelgar@gmail.com>
2021-04-15 09:15:19 +0100

    ffs

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 echo "Setting things up"
-module load languages/anaconda2
 module load languages/gcc/9.1.0
 module load languages/intel/2018-u3
+module load languages/anaconda2/5.0.1
 
 export I_MPI_PMI_LIBRARY=/usr/lib64/libpmi.so
